17f5bebbd29c42927ce7e97bda8dd25f7005fd04
Sbinoto <matthewidantowidantowidanto@gmail.com>
2025-07-25 18:48:27 +0700

    finish exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(Math.floor(a)!=a||Math.floor(b)!=b||a<0||b<0){
+        return "ERROR"
+    }else if(a>b) [a,b]=[b,a];
+    let sum=0;
+    for(a;a<=b;a++){
+        sum+=a
+    }
+    return sum;
 };
 
 // Do not edit below this line
